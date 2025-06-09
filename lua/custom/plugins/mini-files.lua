09c4bbe22021e71510be5e95b501a7f39f98e042
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-06-09 10:47:00 +0200

    Change CWD in mini.files

--- lua/custom/plugins/mini-files.lua
+++ lua/custom/plugins/mini-files.lua
@@ -75,20 +75,38 @@ return {
         local dir_name = vim.fn.fnamemodify(buf_name, ':p:h')
         if vim.fn.filereadable(buf_name) == 1 then
           -- Pass the full file path to highlight the file
-          require('mini.files').open(buf_name, true)
+          mini_files.open(buf_name, true)
         elseif vim.fn.isdirectory(dir_name) == 1 then
           -- If the directory exists but the file doesn't, open the directory
-          require('mini.files').open(dir_name, true)
+          mini_files.open(dir_name, true)
         else
           -- If neither exists, fallback to the current working directory
-          require('mini.files').open(vim.uv.cwd(), true)
+          mini_files.open(vim.uv.cwd(), true)
         end
       end, { desc = 'Open mini.files (Directory of Current File or CWD if not exists)' })
 
       -- Open the current working directory
       vim.keymap.set('n', '<leader>E', function()
-        require('mini.files').open(vim.uv.cwd(), true)
+        mini_files.open(vim.uv.cwd(), true)
       end, { desc = 'Open mini.files (Directory of Current File or CWD if not exists)' })
+
+      -- From https://github.com/echasnovski/mini.nvim/blob/12ebac8280869048c286d13bd35cfa6653e07642/doc/mini-files.txt#L508-L534
+      -- Set focused directory as current working directory
+      local set_cwd = function()
+        local path = (mini_files.get_fs_entry() or {}).path
+        if path == nil then
+          return vim.notify 'Cursor is not on valid entry'
+        end
+        vim.fn.chdir(vim.fs.dirname(path))
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local b = args.data.buf_id
+          vim.keymap.set('n', 'g@', set_cwd, { buffer = b, desc = 'Set cwd' })
+        end,
+      })
     end,
   },
 }
